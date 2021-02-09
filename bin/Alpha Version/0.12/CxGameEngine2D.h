fb97ae7f489c5636a3716e857a9cc2300dfde82b
CxIntlCompany <78689968+CxIntlCompany@users.noreply.github.com>
2021-02-09 14:56:44 +0800

    Add files via upload

---
+++ bin/Alpha Version/0.12/CxGameEngine2D.h
@@ -0,0 +1,35 @@
+#include <Windows.h>
+
+#ifndef CXGAMEENGINE2DDLL
+	#define CXGAMEENGINE2DDLL
+
+#ifdef _DECLSPEC
+	#define DECLSPEC extern "C" __declspec(dllexport)
+#else
+	#define DECLSPEC extern "C" __declspec(dllexport)
+#endif
+
+//Pre Defined
+PAINTSTRUCT paintStruct;
+HDC hdc;
+
+int _rect_index = 0;
+
+struct rect_data {
+	int rect_left, rect_top, rect_right, rect_bottom;
+};
+
+//SetWindowData
+DECLSPEC int set_style(const char* style);
+DECLSPEC int set_hIcon(const char* hIcon_path);
+DECLSPEC int set_windowBackground(const char* bg_type);
+DECLSPEC int set_hCursor(const char* hCursor_type);
+DECLSPEC int set_cbClsExtra(int cbClsExtra);
+DECLSPEC int set_cbWndExtra(int cbWndExtra);
+
+//CreateWindow
+DECLSPEC int createNewWindow(const char* szWindowClass, const char* szTitle, const char* lpszClassName, const char* lpszMenuName);
+
+//Pre Defined Function
+DECLSPEC int createRect(int left, int top, int right, int bottom);
+#endif
